3180a6186761f13f53959559d1177beca5f6c997
TarasYarovyi <yarovoy.taras@gmail.com>
2023-07-18 15:56:59 +0200

    Added querySelectors

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const add = document.querySelector(".add");
+const deleteAll = document.querySelector(".delete-all");
+const noteArea = document.querySelector(".note-area");
+const note = document.querySelector(".note");
+const deleteNote = document.querySelector(".delete-note");
+const notePanel = document.querySelector(".note-panel");
+const category = document.querySelector("#category");
+const text = document.querySelector("#text");
+const error = document.querySelector(".error");
+const save = document.querySelector(".save");
+const cancel = document.querySelector(".cancel");
