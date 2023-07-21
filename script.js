acaca533b8cec72319c0ba8ee1195ba02356af58
TarasYarovyi <yarovoy.taras@gmail.com>
2023-07-21 13:15:27 +0200

    added error message and feature to delete notes

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const addBtn = document.querySelector(".add");
 const deleteAllBtn = document.querySelector(".delete-all");
-const deleteNoteBtns = document.getElementsByClassName("delete-note");
 const saveBtn = document.querySelector(".save");
 const cancelBtn = document.querySelector(".cancel");
 const noteArea = document.querySelector(".note-area");
@@ -12,47 +11,51 @@ const text = document.querySelector("#text");
 const error = document.querySelector(".error");
 const note = document.querySelector(".note");
 
-let selectedValue;
 let cardID = 1;
 
-addBtn.addEventListener("click", () => {
-  notePanel.style.display = "flex";
-});
+addBtn.addEventListener("click", () => (notePanel.style.display = "flex"));
+deleteAllBtn.addEventListener("click", () => (noteArea.innerHTML = ""));
+cancelBtn.addEventListener("click", () => (notePanel.style.display = "none"));
+saveBtn.addEventListener("click", saveNote);
 
-cancelBtn.addEventListener("click", () => {
-  notePanel.style.display = "none";
-});
+function saveNote() {
+  if (category.value !== "0" && text.value) {
+    notePanel.style.display = "none";
+    const newNote = document.createElement("div");
+    newNote.className = "note";
+    newNote.id = cardID;
+    newNote.innerHTML = ` <div class="note-header">
+        <h3 class="note-tittle">Note #${cardID}</h3>
+        <button class="delete-note" onclick="deleteNote(${cardID})">
+          <i class="fas fa-times icon"></i>
+        </button>
+      </div>
+      <div class="note-body">${text.value}</div>`;
 
-saveBtn.addEventListener("click", () => {
-  notePanel.style.display = "none";
-  const newNote = document.createElement("div");
-  newNote.className = "note";
-  newNote.innerHTML = `<div class="note-header">
-  <h3 class="note-tittle">Note #${cardID}</h3>
-  <button class="delete-note">
-    <i class="fas fa-times icon"></i>
-  </button>
-</div>
-<div class="note-body">
-  ${text.value}
-</div>`;
+    switch (category.value) {
+      case "1":
+        newNote.classList.add("shopping");
+        break;
+      case "2":
+        newNote.classList.add("work");
+        break;
+      case "3":
+        newNote.classList.add("other");
+        break;
 
-  switch (category.value) {
-    case "1":
-      newNote.classList.add("shopping");
-      break;
-    case "2":
-      newNote.classList.add("work");
-      break;
-    case "3":
-      newNote.classList.add("other");
-      break;
-
-    default:
-      break;
+      default:
+        break;
+    }
+    noteArea.append(newNote);
+    text.value = "";
+    cardID++;
+    category.value = "0";
+    error.style.visibility = "hidden";
+  } else {
+    error.style.visibility = "visible";
   }
-  noteArea.append(newNote);
-  text.value = "";
-  cardID++;
-  category.value = "0";
-});
+}
+
+function deleteNote(id) {
+  document.getElementById(`${id}`).remove();
+}
